936c9a26e622a0284109469894ffea147f5c0966
B <jirayu.boing99@gmail.com>
2025-06-18 11:30:15 +0700

    first commit

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 # config.py
 class Config:
-    SECRET_KEY = 'your-secret-key'
-    JWT_SECRET_KEY = 'your-jwt-secret'
+    SECRET_KEY = 'your-secret-key'#nigga หน้าดำ
+    JWT_SECRET_KEY = 'your-jwt-secret'#nigga หน้าเผือก
     MONGODB_SETTINGS = {
         'db': 'mario_notes',
         'host': 'localhost',
